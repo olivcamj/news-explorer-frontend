f69d57891074b30eb91095233bce48732b7d160f
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-05-05 15:06:58 -0400

    feat: not found component

---
+++ news-explorer-frontend/src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import notfound from '../../images/not-found.png';
+import './NotFound.css';
+
+function NotFound(props) {
+return (
+  <div className="not-found">
+    <img src={notfound} alt="not found" className="not-found__image" />
+      <h2 className="not-found__heading">Nothing found</h2>
+      <p className="not-found__text">{props.errorMessage !== "" ?  props.errorMessage : "Sorry, but nothing matched your search terms."}</p>
+  </div>
+);
+}
+
+export default NotFound;
