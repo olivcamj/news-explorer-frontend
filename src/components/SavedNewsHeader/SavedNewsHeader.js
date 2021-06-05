dfe2bff18b5b73b6044ac77b5556184401cdab36
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-06-04 22:37:36 -0400

    refactor: add functionality and current user value

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,20 +1,72 @@
 import React from 'react'
 
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 import './SavedNewsHeader.css';
 
 function SavedNewsHeader(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const TOP_TWO = 2;
+  const ZERO_ITEMS = 0;
+  const THREE_ITEMS = 3;
+  let list = [];
+  const frequentKeywordsList = () => {
+    // go through all the keywords in saved cards array
+    let savedCards = props.cards;
+    if (savedCards) {
+      // eslint-disable-next-line
+      savedCards.map((el) => {
+        list.push(el.keyword);
+      });
+    }
+    return list;
+  };
+
+  // find the top 2 most frequent terms
+  const findMostFrequentKeywords = (keywords) => {
+     let hash = {};
+     for (let word of keywords) {
+       if (!hash[word]) hash[word] = ZERO_ITEMS;
+       hash[word]++;
+     }
+     const hashToArray = Object.entries(hash);
+     const sortedArray = hashToArray.sort((a, b) => b[1] - a[1]);
+     const sortedElements = sortedArray.map((word) => word[0]);
+     return sortedElements;
+  }
+
+  const topFrequentKeywords = (keywordsArray, k) => {
+    const topKeywords = findMostFrequentKeywords(keywordsArray);
+    const mostFrequent = topKeywords.slice(ZERO_ITEMS, k);
+    return mostFrequent.join(', ');
+  }
+
+  const amountOfSavedCards = () => {
+    if ((props.cards.length > ZERO_ITEMS) && props.cards.length > TOP_TWO) {
+      // Using the list of frequent keywords return the occurence of the least popular keywords
+      let arr = findMostFrequentKeywords(frequentKeywordsList());
+      let remainingAmount = arr.slice(TOP_TWO).length;
+      let moreKeywords = props.cards.length > THREE_ITEMS ? "others" : "other";
+      return `, and ${remainingAmount} ${moreKeywords}`;
+    }
+  }
+
   return (
     <div className="saved-news-header">
       <h2 className="saved-news-header__title">Saved Articles</h2>
-      <h3 className="saved-news-header__subtitle">Elise, you have 5 saved articles</h3>
+      <h3 className="saved-news-header__subtitle">
+        {currentUser && currentUser.name}, you have {props.cards.length} saved
+        articles
+      </h3>
       <p className="saved-news-header__keyword-list">
-        By keywords: 
+        By keywords:
         <span className="saved-news-header__keyword">
-           Nature, Yellowstone, and 2 other
+          {topFrequentKeywords(frequentKeywordsList(), TOP_TWO)}
+          {amountOfSavedCards()}
         </span>
       </p>
     </div>
-  )
+  );
 }
 
 export default SavedNewsHeader;
