ebb0113ab572c10bb0043ab2c4cf225c7246b3ce
Olivia Cameron <olivia.cameronj15@gmail.com>
2021-07-07 18:59:13 -0400

    style: add semicolons and fix spacing

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import './Preloader.css'
-import preloader from '../../images/Ellipse.png'
+import React from 'react';
+import './Preloader.css';
+import preloader from '../../images/Ellipse.png';
 
 function Preloader() {
-    return (
-      <div className="preloader">
-        <img
-          src={preloader}
-          alt="loading"
-          className="preloader__image circle-preloader"
-        />
-        <p className="preloader__text">Searching for news...</p>
-      </div>
-    );
+  return (
+    <div className="preloader">
+      <img
+        src={preloader}
+        alt="loading"
+        className="preloader__image circle-preloader"
+      />
+      <p className="preloader__text">Searching for news...</p>
+    </div>
+  );
 }
 
 export default Preloader;
